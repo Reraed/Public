546a750afe5eceb8921f7a451dcc08b8cecaad75
elyaa <issaelyaa@gmail.com>
2021-04-21 09:58:17 -0400

    changed to my own email

--- link/src/link/Class1.java
+++ link/src/link/Class1.java
@@ -1,5 +1,20 @@
+/**
+ * 
+ */
 package link;
 
+/**
+ * @author ahmad
+ *
+ */
 public class Class1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
